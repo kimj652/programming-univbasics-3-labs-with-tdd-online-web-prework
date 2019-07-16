ea7dc2631f63bd392d8664fa5cebc3111cd9e244
Judy Kim <kim.ju652@gmail.com>
2019-07-16 05:52:07 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
-first_number = 0
-second_number = 0
+first_number = 5
+second_number = 3
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
